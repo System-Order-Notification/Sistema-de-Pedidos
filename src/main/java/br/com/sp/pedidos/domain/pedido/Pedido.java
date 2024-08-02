1618f9b8a0bfdcfcd1b3ad2381cd1fc6c6f5ce4b
AndrewAscar742 <andrewmatosascar@outlook.com>
2024-08-02 17:14:20 -0300

    fix: Ajustes na Entidade

--- src/main/java/br/com/sp/pedidos/domain/pedido/Pedido.java
+++ src/main/java/br/com/sp/pedidos/domain/pedido/Pedido.java
@@ -1,12 +1,16 @@
 package br.com.sp.pedidos.domain.pedido;
 
 import java.time.LocalDateTime;
+
+import java.lang.Boolean;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import br.com.sp.pedidos.domain.PedidoItem;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +24,8 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
+	
+	@Column(nullable = false, updatable = false)
 	private UUID userID;
 	private LocalDateTime dataHora;
 	private String descricao;
@@ -27,27 +33,29 @@ public class Pedido {
 	private Float valorFrete;
 	private Float valorTotalGeral;
 	private MetodoPagamento metodoPagamento;
+	private Boolean actived;
 	
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<PedidoItem> itens = new ArrayList<PedidoItem>();
 	
 	public Pedido() {
-		// TODO Auto-generated constructor stub
+		this.actived = true;
 	}
 
-	public Pedido(UUID userID, LocalDateTime dataHora, String descricao, Float valorTotal, Float valorFrete,
-			Float valorTotalGeral, MetodoPagamento metodoPagamento, List<PedidoItem> itens) {
+	public Pedido(UUID userID, String descricao, Float valorTotal, Float valorFrete,
+			Float valorTotalGeral, MetodoPagamento metodoPagamento,  List<PedidoItem> itens) {
 		super();
 		this.userID = userID;
-		this.dataHora = dataHora;
+		this.dataHora = LocalDateTime.now();
 		this.descricao = descricao;
 		this.valorTotal = valorTotal;
 		this.valorFrete = valorFrete;
 		this.valorTotalGeral = valorTotalGeral;
 		this.metodoPagamento = metodoPagamento;
+		this.actived = true;
 		this.itens = itens;
 	}
-
+ 
 	public UUID getId() {
 		return id;
 	}
@@ -119,6 +127,13 @@ public class Pedido {
 	public void setItens(List<PedidoItem> itens) {
 		this.itens = itens;
 	}
-	
+
+	public Boolean getActived() {
+		return actived;
+	}
+
+	public void setActived(Boolean actived) {
+		this.actived = actived;
+	}
 	
 }
